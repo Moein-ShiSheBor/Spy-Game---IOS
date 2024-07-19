99c4dbd1c94cd3dc3e0baa668c1bc38033fa8b78
Moein-ShiSheBor <shisheborm79@gmail.com>
2024-07-19 13:02:24 +0330

    Initial Commit

---
+++ Spy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spy
+//
+//  Created by moein shishebor on 2024-07-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spy/SpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyApp.swift
+//  Spy
+//
+//  Created by moein shishebor on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct SpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
